6b3aaac370a8268f295cdf7e9f609b34cc8e1ebe
Alec Girman <agirman@lexile.com>
2021-02-05 12:09:35 -0500

    Added admin command to enable and disable announcements

--- backend/announcements/admin.py
+++ backend/announcements/admin.py
@@ -5,8 +5,23 @@ from django.contrib import admin
 from announcements.models import Announcement
 
 
+def enable_announcement(modeladmin, request, queryset):
+    queryset.update(enabled=True)
+
+
+enable_announcement.short_description = "Enable the selected announcements"
+
+
+def disable_announcement(modeladmin, request, queryset):
+    queryset.update(enabled=True)
+
+
+disable_announcement.short_description = "Disable the selected announcements"
+
+
 class AnnouncementAdmin(admin.ModelAdmin):
     list_display = ["enabled", "title", "day", "time", "channel", "message"]
+    actions = [enable_announcement, disable_announcement]
 
 
 admin.site.register(Announcement, AnnouncementAdmin)
